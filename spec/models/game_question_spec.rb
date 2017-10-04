174118b80ff0a2e04b2c3ef8bacb54ffe52d7bef
0legovich <romq1@mail.ru>
2017-10-05 01:32:42 +0300

    refractor rspec game_question_spec.rb

--- spec/models/game_question_spec.rb
+++ spec/models/game_question_spec.rb
@@ -5,8 +5,8 @@ RSpec.describe GameQuestion, type: :model do
   let(:game_question) {FactoryGirl.create(:game_question, a: 2, b: 1, c: 4, d: 3)}
 
   #Проверяем корректность методов связанных с ответами
-  context 'check methods' do
-    it 'correct .variants' do
+  describe '#variants' do
+    it 'get correct answer variants' do
       expect(game_question.variants).to eq(
                                           {
                                             'a' => game_question.question.answer2,
@@ -16,25 +16,39 @@ RSpec.describe GameQuestion, type: :model do
                                           }
                                         )
     end
+  end
 
-    it 'correct .answer_correct?' do
-      expect(game_question.answer_correct?('b')).to be_truthy
+  describe '#answer_correct?' do
+    context 'when answer is correct' do
+      it 'return true' do
+        expect(game_question.answer_correct?('b')).to be_truthy
       end
-    it 'correct .correct_answer_key' do
+    end
+  end
+
+  describe '#correct_answer_key' do
+    it 'get correct answer key' do
       expect(game_question.correct_answer_key).to eq 'b'
     end
   end
 
   #проверяем delegate методы
-  context 'check delegate methods' do
-    it 'there is .text .level' do
-      expect {game_question.text}.not_to raise_exception
-      expect {game_question.level}.not_to raise_exception
+  describe 'delegate methods' do
+    describe '#text' do
+      it 'there is' do
+        expect {game_question.text}.not_to raise_exception
+      end
+    end
+    describe '#level' do
+      it 'there is' do
+        expect {game_question.level}.not_to raise_exception
+      end
     end
   end
 
-  context 'user_helpers' do
-    it 'correct audience_help' do
+  # проверяем user_helpers
+  describe '#add_audience_help' do
+    it 'add ":audience_help" in help_hash' do
       expect(game_question.help_hash).not_to include(:audience_help)
 
       game_question.add_audience_help
